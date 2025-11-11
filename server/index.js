d2cc4495fb2c6167762ed87deb709400ab785086
Sikander Riaz <sikanderriazkhan@gmail.com>
2025-11-11 12:20:11 +0500

    feat:prom client metrics addition

--- server/index.js
+++ server/index.js
@@ -1,43 +1,58 @@
-const { ApolloServer } = require('apollo-server');
-const mongoose = require('mongoose');
-const { MONGODB } = require('./config');
 
-const typeDefs = require('./graphql/Schema');
-const resolvers = require('./graphql/resolvers');
-const { authMiddleware } = require('./utils/authMiddleware');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const mongoose = require("mongoose");
+const client = require("prom-client");
+const { MONGODB } = require("./config");
+const typeDefs = require("./graphql/Schema");
+const resolvers = require("./graphql/resolvers");
+const { authMiddleware } = require("./utils/authMiddleware");
+
+const app = express();
+
+// --- PROMETHEUS SETUP ---
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+const gqlRequests = new client.Counter({
+  name: "graphql_requests_total",
+  help: "Total number of GraphQL requests",
+});
+register.registerMetric(gqlRequests);
+
+// Metrics endpoint
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
+// -------------------------
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: (context) => {
-    const me = authMiddleware(context);
-
-    return {
-      me,
-      context,
-    };
+  context: ({ req }) => {
+    const me = authMiddleware(req);
+    gqlRequests.inc();
+    return { me };
   },
-  // formatError: error => {
-  //   // remove the internal error message
-  //   // leave only the important validation error
-  //   const message = error.message
-  //     .replace('Validation error: ', '');
-
-  //   return {
-  //     ...error,
-  //     message,
-  //   };
-  // },
 });
 
-mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
-  .then((res) => {
-    console.log('MongoDb Connected');
-    return server.listen({ port: 5000 });
-  })
-  .then((res) => {
-    console.log(`Server running at ${res.url}`);
-  });
+async function startServer() {
+  await server.start();
 
-// createpost, login is done
+  // Mount Apollo Server at "/"
+  server.applyMiddleware({ app, path: "/" });
+
+  mongoose
+    .connect(MONGODB, { useNewUrlParser: true })
+    .then(() => {
+      console.log("MongoDB Connected");
+      app.listen({ port: 5000 }, () => {
+        console.log(` Server ready at http://localhost:5000/`);
+        console.log(`Metrics available at http://localhost:5000/metrics`);
+      });
+    })
+    .catch((err) => console.error(err));
+}
+
+startServer();
