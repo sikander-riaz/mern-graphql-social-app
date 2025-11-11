09b84558e1ee272785a82832c449102c661c215e
Sikander Riaz <sikanderriazkhan@gmail.com>
2025-11-11 12:17:30 +0500

    fix:route for mongodb

--- server/config.js
+++ server/config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  MONGODB: 'mongodb://localhost:27017/merng-social-app',
-  SECRET_KEY: 'MY_SECRET_KEY',
+  // MONGODB: 'mongodb://localhost:27017/merng-social-app',
+
+  MONGODB:
+    "mongodb+srv://db_user:qxW1tQj4nRR8Tpma@cluster0.xijxjzn.mongodb.net/?appName=Cluster0",
+  SECRET_KEY: "MY_SECRET_KEY",
 };
